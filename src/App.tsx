2a229507e282f785c51cfa5103d6be1f4506fb0c
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2023-03-05 12:31:36 +0545

    client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,261 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Row, Col, Button, Spin, List, Checkbox, Input } from "antd";
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { AptosClient } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useState, useEffect } from "react";
+
+const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
+const client = new AptosClient(NODE_URL);
+
+const moduleAddress = "0xa1e3aa355555eb0b94cb3de6c98c1a5dd33c45a3647b3b5697c21d4719339b2e";
+
+type Task = {
+  address: string;
+  completed: boolean;
+  content: string;
+  task_id: string;
+};
 
 function App() {
+  const { account, signAndSubmitTransaction } = useWallet();
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+  const [transactionInProgress, setTransactionInProgress] = useState<boolean>(false);
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const onWriteTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setNewTask(value);
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+
+
+  const fetchList = async () => {
+    if (!account) return [];
+    try {
+      const CandyMachineResource = await client.getAccountResource(
+        account.address,
+        `${moduleAddress}:candymachine::CandyMachine`
+        
+      );
+      setAccountHasList(true);
+      const tableHandle = (CandyMachineResource as any).data.tasks.handle;
+      const taskCounter = (CandyMachineResource as any).data.task_counter;
+
+      let tasks = [];
+      let counter = 1;
+      while (counter <= taskCounter) {
+        const tableItem = {
+          key_type: "u64",
+          value_type: `${moduleAddress}::candymachine::Task`,
+          key: `${counter}`,
+        };
+        const task = await client.getTableItem(tableHandle, tableItem);
+        tasks.push(task);
+        counter++;
+      }
+          // set tasks in local state
+      setTasks(tasks);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+    console.log("Hello");
+
+  };
+ 
+  const addNew = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::candymachine::init_candy`,
+      type_arguments: [],
+      arguments: [],
+    };
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
+  const onTaskAdded = async () => {
+    // check for connected account
+    if (!account) return;
+    setTransactionInProgress(true);
+    // build a transaction payload to be submited
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::candymachine::init_candy`,
+      type_arguments: [],
+      arguments: [newTask],
+    };
+
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+
+            // hold the latest task.task_id from our local state
+      const latestId = tasks.length > 0 ? parseInt(tasks[tasks.length - 1].task_id) + 1 : 1;
+
+      // build a newTaskToPush objct into our local state
+      const newTaskToPush = {
+        address: account.address,
+        completed: false,
+        content: newTask,
+        task_id: latestId + "",
+      };
+
+      // Create a new array based on current state:
+      let newTasks = [...tasks];
+
+      // Add item to it
+      newTasks.unshift(newTaskToPush);
+
+      // Set state
+      setTasks(newTasks);
+            // clear input text
+      setNewTask("");
+    } catch (error: any) {
+      console.log("error", error);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
+  const onCheckboxChange = async (
+    event: CheckboxChangeEvent,
+    taskId: string
+  ) => {
+    if (!account) return;
+    if (!event.target.checked) return;
+    setTransactionInProgress(true);
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::candymachine::init_candy`,
+      type_arguments: [],
+      arguments: [taskId],
+    };
+
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+
+      setTasks((prevState) => {
+        const newState = prevState.map((obj) => {
+          // if task_id equals the checked taskId, update completed property
+          if (obj.task_id === taskId) {
+            return { ...obj, completed: true };
+          }
+
+          // otherwise return object as is
+          return obj;
+        });
+
+        return newState;
+      });
+    } catch (error: any) {
+      console.log("error", error);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Layout>
+      <Row align="middle">
+        <Col span={10} offset={2}>
+          <h1>Candymachine</h1>
+        </Col>
+        <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+          <WalletSelector />
+        </Col>
+      </Row>
+    </Layout>
+    <Spin spinning={transactionInProgress}>
+        {!accountHasList ? (
+          <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+            <Col span={8} offset={8}>
+              <Button
+                disabled={!account}
+                block
+                onClick={addNew}
+                type="primary"
+                style={{ height: "40px", backgroundColor: "#3f67ff" }}
+              >
+                Add new
+              </Button>
+            </Col>
+          </Row>
+        ) : (
+          <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+            <Col span={8} offset={8}>
+              <Input.Group compact>
+                <Input
+                  onChange={(event) => onWriteTask(event)}
+                  style={{ width: "calc(100% - 60px)" }}
+                  placeholder="Add a Task"
+                  size="large"
+                  value={newTask}
+                />
+                <Button onClick={onTaskAdded} type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
+                  Add
+                </Button>
+              </Input.Group>
+            </Col>
+            <Col span={8} offset={8}>
+              {tasks && (
+                <List
+                  size="small"
+                  bordered
+                  dataSource={tasks}
+                  renderItem={(task: Task) => (
+                    <List.Item
+                      actions={[
+                        <div>
+                          {task.completed ? (
+                            <Checkbox defaultChecked={true} disabled />
+                          ) : (
+                            <Checkbox onChange={(event) => onCheckboxChange(event, task.task_id)} />
+                          )}
+                        </div>,
+                      ]}
+                    >
+                      <List.Item.Meta
+                        title={task.task_id}
+                        description={
+                          <a
+                            href={`https://explorer.aptoslabs.com/account/${task.address}/`}
+                            target="_blank"
+                          >{`${task.address.slice(0, 6)}...${task.address.slice(-5)}`}</a>
+                        }
+                      />
+                    </List.Item>
+                  )}
+                />
+              )}
+            </Col>
+          </Row>
+        )}
+      </Spin>
+  </>
   );
 }
 

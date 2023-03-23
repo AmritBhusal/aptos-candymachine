0767a16eadd508f44936ed695ed645750d3d6a1d
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2023-03-23 15:55:09 +0545

    Mint_candy trying diffrent approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import  Mint  from './Components/Miniting'
-import Init from './Components/Initial';
+// import  Mint  from './Components/Miniting'
+// import Init from './Components/Initial';
+// import Mint_candy from './Components/Mint_candy';
 
 const App = () => {
   return (
     <div>
       {/* <Init /> */}
-      <Mint />
+      {/* <Mint /> */}
+      {/* <Mint_candy /> */}
     </div>
   )
 }
